1b9cd84cbdcb15b05887b57f6b1689b9bf6b7fa8
Marcelo Dias <diasteixeira1990@gmail.com>
2023-08-17 14:16:27 -0300

    refatoração: criado novo metodo

--- src/data/usecases/add-account/db-account.spec.ts
+++ src/data/usecases/add-account/db-account.spec.ts
@@ -1,14 +1,28 @@
+import { Criptografar } from "../../protocols/encrypter";
 import { AdicionarUsuarioDB } from "../db-add-account";
+
+interface SutTypes {
+  criptografarSenha: Criptografar;
+  addUsuarioDB: AdicionarUsuarioDB;
+}
+
+const controller = (): SutTypes => {
+  class CriptografarSenha {
+    async criptografar(valor: string): Promise<string> {
+      return new Promise((resolve) => resolve("senha_criptografada"));
+    }
+  }
+  const criptografarSenha = new CriptografarSenha();
+  const addUsuarioDB = new AdicionarUsuarioDB(criptografarSenha);
+  return {
+    criptografarSenha,
+    addUsuarioDB,
+  };
+};
 
 describe("DbAddAccount Usecase", () => {
   test("Deve ser enviado a senha correta para o metodo", async () => {
-    class CriptografarSenha {
-      async criptografar(valor: string): Promise<string> {
-        return new Promise((resolve) => resolve("senha_criptografada"));
-      }
-    }
-    const criptografarSenha = new CriptografarSenha();
-    const addUsuarioDB = new AdicionarUsuarioDB(criptografarSenha);
+    const { criptografarSenha, addUsuarioDB } = controller();
     const senhaCriptografada = jest.spyOn(criptografarSenha, "criptografar");
     const ususario = {
       nome: "valid_nome",
