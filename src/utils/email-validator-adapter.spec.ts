9020083401d294333d36cc4f66a2c2b87e653bda
Marcelo Dias <diasteixeira1990@gmail.com>
2023-08-16 23:43:26 -0300

    refatoração novo metodo controller

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -7,22 +7,26 @@ jest.mock("validator", () => ({
   },
 }));
 
+const controller = (): ValidarEmailAdaptado => {
+  return new ValidarEmailAdaptado();
+};
+
 describe("EmailValidator Adapter", () => {
   test("Deve retornar falso se o validação retorna falso", () => {
-    const validarEmailAdaptado = new ValidarEmailAdaptado();
+    const validarEmailAdaptado = controller();
     jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
     const valido = validarEmailAdaptado.valida("invalid_email@mail.com");
     expect(valido).toBe(false);
   });
 
   test("Deve retornar true se a validação retorna true", () => {
-    const validarEmailAdaptado = new ValidarEmailAdaptado();
+    const validarEmailAdaptado = controller();
     const valido = validarEmailAdaptado.valida("invalid_email@mail.com");
     expect(valido).toBe(true);
   });
 
   test("Deve passar para validação o email correto", () => {
-    const validarEmailAdaptado = new ValidarEmailAdaptado();
+    const validarEmailAdaptado = controller();
     const isEmailSpy = jest.spyOn(validator, "isEmail");
     validarEmailAdaptado.valida("any_email@mail.com");
     expect(isEmailSpy).toHaveBeenCalledWith("any_email@mail.com");
