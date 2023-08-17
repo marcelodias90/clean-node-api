c3e06e2c0c510a00579b556fd7ecb84395c80c53
Marcelo Dias <diasteixeira1990@gmail.com>
2023-08-16 23:34:23 -0300

    Novo teste emaiValidator se  retornar true a validação tamber retorna true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,23 @@
 import { ValidarEmailAdaptado } from "./email-validator";
+import validator from "validator";
+
+jest.mock("validator", () => ({
+  isEmail(): boolean {
+    return true;
+  },
+}));
 
 describe("EmailValidator Adapter", () => {
   test("Deve retornar falso se o validação retorna falso", () => {
     const validarEmailAdaptado = new ValidarEmailAdaptado();
+    jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
     const valido = validarEmailAdaptado.valida("invalid_email@mail.com");
     expect(valido).toBe(false);
+  });
+
+  test("Deve retornar true se o validação retorna true", () => {
+    const validarEmailAdaptado = new ValidarEmailAdaptado();
+    const valido = validarEmailAdaptado.valida("invalid_email@mail.com");
+    expect(valido).toBe(true);
   });
 });
