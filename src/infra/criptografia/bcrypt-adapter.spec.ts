182d3dd0b0ba543e0ef13043ac0da2e7b3842a75
Marcelo Dias <diasteixeira1990@gmail.com>
2023-08-18 13:58:49 -0300

    teste retorna a hash se correto

--- src/infra/criptografia/bcrypt-adapter.spec.ts
+++ src/infra/criptografia/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from "bcrypt";
 import { BcryptAdapter } from "./bcrypt-adapter";
 
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve("hash"));
+  },
+}));
+
 describe("Bcrypt Adapter", () => {
   test("Deve passar os valores correto para Bcrypt", async () => {
     const salt = 12;
@@ -8,5 +14,12 @@ describe("Bcrypt Adapter", () => {
     const hahsSpy = jest.spyOn(bcrypt, "hash");
     await controller.criptografar("any_value");
     expect(hahsSpy).toHaveBeenCalledWith("any_value", salt);
+  });
+
+  test("Deve retorna a hash  caso tive correto", async () => {
+    const salt = 12;
+    const controller = new BcryptAdapter(salt);
+    const hash = await controller.criptografar("any_value");
+    expect(hash).toBe("hash");
   });
 });
