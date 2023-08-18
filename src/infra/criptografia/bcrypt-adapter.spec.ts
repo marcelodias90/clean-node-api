76bcb3e75115d3bb02c40ee9daf125de7fc61f2f
Marcelo Dias <diasteixeira1990@gmail.com>
2023-08-18 14:06:36 -0300

    refatoração makeSut

--- src/infra/criptografia/bcrypt-adapter.spec.ts
+++ src/infra/criptografia/bcrypt-adapter.spec.ts
@@ -7,18 +7,21 @@ jest.mock("bcrypt", () => ({
   },
 }));
 
+const salt = 12;
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt);
+};
+
 describe("Bcrypt Adapter", () => {
   test("Deve passar os valores correto para Bcrypt", async () => {
-    const salt = 12;
-    const controller = new BcryptAdapter(salt);
+    const controller = makeSut();
     const hahsSpy = jest.spyOn(bcrypt, "hash");
     await controller.criptografar("any_value");
     expect(hahsSpy).toHaveBeenCalledWith("any_value", salt);
   });
 
   test("Deve retorna a hash  caso tive correto", async () => {
-    const salt = 12;
-    const controller = new BcryptAdapter(salt);
+    const controller = makeSut();
     const hash = await controller.criptografar("any_value");
     expect(hash).toBe("hash");
   });
