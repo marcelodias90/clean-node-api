89339dcea3ed34f235857776d08e5c608991dda0
Marcelo Dias <diasteixeira1990@gmail.com>
2023-08-19 13:15:39 -0300

    refatoração: movido metodos do mongodb pra helpers

---
+++ src/infra/db/mongodb/helpers/mongo-helpers.ts
@@ -0,0 +1,15 @@
+import { MongoClient } from "mongodb";
+
+export const MongoHelper = {
+  client: null as MongoClient,
+
+  async connect(url: string): Promise<void> {
+    this.client = await MongoClient.connect(url, {
+      directConnection: true,
+    });
+  },
+
+  async disconnect(): Promise<void> {
+    await this.client.close();
+  },
+};
